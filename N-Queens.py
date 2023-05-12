72c747b738165c4f00f5c998b7c9bff8f8af016e
Mihir Maheshwari <95093943+m-Qube@users.noreply.github.com>
2023-05-12 08:19:02 +0530

    Create N-Queens.py

---
+++ N-Queens.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        col = set()
+        posDiag = set()
+        negDiag = set()
+
+        ans = []
+        board = [['.'] * n for i in range(n)]
+
+        def backtrack(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                ans.append(copy)
+                return
+            
+            for c in range(n):
+                if c in col or (r+c) in posDiag or (r-c) in negDiag:
+                    continue
+
+                col.add(c)
+                posDiag.add(r+c)
+                negDiag.add(r-c)
+                board[r][c] = 'Q'
+
+                backtrack(r+1)
+                col.remove(c)
+                posDiag.remove(r+c)
+                negDiag.remove(r-c)
+                board[r][c] = '.'
+                
+        backtrack(0)
+        return ans
